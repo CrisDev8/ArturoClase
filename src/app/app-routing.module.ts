cfe605c1b3109b8409807ac7ba56bb85fd55a667
Cristian Silva <cristiandev8@gmail.com>
2021-01-18 20:46:31 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: LoginComponent,
+    path: '',
+  },
+  {
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+    path: 'home',
+  },
+  {
+    loadChildren: () =>
+      import('./pages/news/news.module').then((m) => m.NewsModule),
+    path: 'news',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
